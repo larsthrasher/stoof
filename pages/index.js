359edc67728e031eddc9925473803bde2e4dc132
larsthrasher <thrasherlars@gmail.com>
2020-01-12 20:18:15 -0500

    items rendering

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,10 @@
-import React from 'react'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
+import ProductList from '../components/Index/ProductList'
 
 
 function Home({ products }) {
-  console.log(products)
-  // React.useEffect(() => {
-  //   getProducts()
-  // }, [])
-  //
-  // async function getProducts() {
-  //   const url = 'http://localhost:3000/api/products';
-  //   const response = await axios.get(url);
-  //   console.log(response.data);
-  // }
-  return <>home</>;
+  return <ProductList products={products} />;
 }
 
 Home.getInitialProps = async () => {
@@ -22,7 +13,6 @@ Home.getInitialProps = async () => {
   const response = await axios.get(url);
   return { products: response.data };
   // return response data as an object
-  return { hello: 'world' }
   // note: this object will be merged with exisiting props
 }
 
