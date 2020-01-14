fb880bef568d169077da610efde3de4bf0d3be83
larsthrasher <thrasherlars@gmail.com>
2020-01-14 14:40:10 -0500

    user set in database

--- pages/signup.js
+++ pages/signup.js
@@ -6,8 +6,12 @@ import {
   Message,
   Segment
 } from 'semantic-ui-react'
-import Link from 'next/link'
-import catchErrors from '../utils/catchErrors'
+import Link from 'next/link';
+import axios from 'axios';
+import catchErrors from '../utils/catchErrors';
+import baseUrl from '../utils/baseUrl';
+import { handleLogin } from '../utils/auth';
+
 
 const INITIAL_USER = {
   name: "",
@@ -38,7 +42,10 @@ function Signup() {
       // make request to signup user
       setLoading(true);
       setError('');
-      console.log(user)
+      const url = `${baseUrl}/api/signup`
+      const payload = { ...user }
+      const response = await axios.post(url, payload)
+      handleLogin(response.data)
     } catch(error) {
       catchErrors(error, setError);
     } finally {
