d746664316c0c944a343997c334536351d868ff1
larsthrasher <thrasherlars@gmail.com>
2020-01-14 13:02:48 -0500

    signup/login forms created

--- pages/create.js
+++ pages/create.js
@@ -59,8 +59,8 @@ async function handleSubmit(event) {
   try {
     event.preventDefault();
     setLoading(true)
+    setError('')
     const mediaUrl = await handleImageUpload();
-    console.log({ mediaUrl });
     const url = `${baseUrl}/api/product`
     const { name, price, description } = product
     const payload = { name, price, description, mediaUrl };
