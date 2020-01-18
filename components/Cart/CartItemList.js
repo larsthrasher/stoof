1cb2fbf3e494ecf0c9a27543f498beee475f81bc
larsthrasher <thrasherlars@gmail.com>
2020-01-18 18:52:28 -0500

    stripe implemented

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,7 +1,7 @@
-import { Header, Segment, Button, Icon, Item } from 'semantic-ui-react';
+import { Header, Segment, Button, Icon, Item, Message } from 'semantic-ui-react';
 import { useRouter } from 'next/router'
 
-function CartItemList({ products, user, handleRemoveFromCart }) {
+function CartItemList({ products, user, handleRemoveFromCart, success }) {
   const router = useRouter()
 
   function mapCartProductsToItems(products) {
@@ -23,7 +23,18 @@ function CartItemList({ products, user, handleRemoveFromCart }) {
           onClick={() => handleRemoveFromCart(p.product._id)}
          />
       )
-    }))
+    }));
+  }
+
+  if (success) {
+    return (
+      <Message
+        success
+        header="Success!"
+        content="Your order and payment has been accepted"
+        icon="star outline"
+      />
+    )
   }
 
   if (products.length === 0) {
--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout'
 import { Button, Segment, Divider } from 'semantic-ui-react';
 import calculateCartTotal from '../../utils/calculateCartTotal'
 
 
-function CartSummary({ products }) {
+function CartSummary({ products, handleCheckout, success }) {
   const [cartAmount, setCartAmount] = React.useState(0)
   const [stripeAmount, setStripeAmount] = React.useState(0)
   const [isCartEmpty, setCartEmpty] = React.useState(false)
@@ -15,18 +16,32 @@ function CartSummary({ products }) {
     setCartEmpty(products.length === 0);
   }, [products]);
 
+
   return (
     <>
       <Divider />
       <Segment clearing size="large">
         <strong>Sub total:</strong> ${cartAmount}
+        <StripeCheckout
+          name="React Reserve"
+          amount={stripeAmount}
+          image={products.length > 0 ? products[0].product.mediaUrl : ""}
+          currency="USD"
+          shippingAddress={true}
+          billingAddress={true}
+          zipCode={true}
+          stripeKey="pk_test_hdTxpLbXKHsmBqR1rVnVeffo00Q3LhEnoO"
+          token={handleCheckout}
+          triggerEvent="onClick"
+        >
         <Button
           icon="cart"
-          disabled={isCartEmpty}
+          disabled={isCartEmpty || success}
           color="teal"
           floated="right"
           content="Checkout"
         />
+        </StripeCheckout>
       </Segment>
     </>
   );
