f258aa5e2c69e8e02006ba544f4edfac9d19a3d8
larsthrasher <thrasherlars@gmail.com>
2020-01-19 15:49:19 -0500

    commit

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken';
-import Cart from '../../models/Cart';
-import connectDb from '../../utils/connectDb';
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import Cart from "../../models/Cart";
+import connectDb from "../../utils/connectDb";
 
 connectDb();
 
-const { ObjectId } = mongoose.Types
+const { ObjectId } = mongoose.Types;
 
 export default async (req, res) => {
-  switch(req.method){
+  switch (req.method) {
     case "GET":
       await handleGetRequest(req, res);
       break;
@@ -19,82 +19,89 @@ export default async (req, res) => {
       await handleDeleteRequest(req, res);
       break;
     default:
-      res.status(405).send(`Method ${req.method} not allowed`)
+      res.status(405).send(`Method ${req.method} not allowed`);
       break;
   }
-}
+};
 
-async function handleGetRequest(req,res) {
-  if(!('authorization' in req.headers)) {
-    return res.status(401).send('No authorization token');
+async function handleGetRequest(req, res) {
+  if (!("authorization" in req.headers)) {
+    return res.status(401).send("No authorization token");
   }
   try {
-    const { userId } = jwt.verify(req.headers.authorization,
-    process.env.JWT_SECRET);
+    const { userId } = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
     const cart = await Cart.findOne({ user: userId }).populate({
       path: "products.product",
       model: "Product"
-    })
-    res.status(200).json(cart.products)
+    });
+    res.status(200).json(cart.products);
   } catch (error) {
-    console.error(error)
-    res.status(403).send("Please login again")
+    console.error(error);
+    res.status(403).send("Please login again");
   }
 }
 
 async function handlePutRequest(req, res) {
-  const { quantity, productId } = req.body
-  if(!('authorization' in req.headers)) {
-    return res.status(401).send('No authorization token');
+  const { quantity, productId } = req.body;
+  if (!("authorization" in req.headers)) {
+    return res.status(401).send("No authorization token");
   }
   try {
-    const { userId } = jwt.verify(req.headers.authorization,
-    process.env.JWT_SECRET);
-    // get user cart based on userId
-    const cart = await Cart.findOne({ user: userId })
-    // check if product already exists in cart
-    const productExists = cart.products.some(doc => ObjectId
-      (productId).equals(doc.product))
-    // if so, increment quantity by number provided to in request
+    const { userId } = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    // Get user cart based on userId
+    const cart = await Cart.findOne({ user: userId });
+    // Check if product already exists in cart
+    const productExists = cart.products.some(doc =>
+      ObjectId(productId).equals(doc.product)
+    );
+    // If so, increment quantity (by number provided to request)
     if (productExists) {
       await Cart.findOneAndUpdate(
         { _id: cart._id, "products.product": productId },
-        {$inc: { 'products.$.quantity': quantity }}
-      )
+        { $inc: { "products.$.quantity": quantity } }
+      );
     } else {
-      const newProduct = { quantity, product: productId }
+      // If not, add new product with given quantity
+      const newProduct = { quantity, product: productId };
       await Cart.findOneAndUpdate(
-        { _id: cart._id},
-        { $addToSet: { products: newProduct }}
-      )
+        { _id: cart._id },
+        { $addToSet: { products: newProduct } }
+      );
     }
-    res.status(200).send("Cart updated")
-    // if not, add new product with given quantity
+    res.status(200).send("Cart updated");
   } catch (error) {
-    console.error(error)
-    res.status(403).send("Please login again")
+    console.error(error);
+    res.status(403).send("Please login again");
   }
 }
 
 async function handleDeleteRequest(req, res) {
-  const { productId } = req.query
-  if(!('authorization' in req.headers)) {
-    return res.status(401).send('No authorization token');
-}
+  const { productId } = req.query;
+  if (!("authorization" in req.headers)) {
+    return res.status(401).send("No authorization token");
+  }
   try {
-    const { userId } = jwt.verify(req.headers.authorization,
-    process.env.JWT_SECRET);
+    const { userId } = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
     const cart = await Cart.findOneAndUpdate(
       { user: userId },
-      { $pull: { products: { product: productId }}},
+      { $pull: { products: { product: productId } } },
       { new: true }
     ).populate({
-      path:"products.product",
-      model:"Product"
-    })
+      path: "products.product",
+      model: "Product"
+    });
     res.status(200).json(cart.products);
   } catch (error) {
     console.error(error);
-    res.status(403).send("Please login again")
+    res.status(403).send("Please login again");
   }
 }
--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,23 +1,24 @@
-import Order from '../../models/Order'
-import jwt from 'jsonwebtoken'
-import connectDb from '../../utils/connectDb'
+import Order from "../../models/Order";
+import jwt from "jsonwebtoken";
+import connectDb from "../../utils/connectDb";
 
-connectDb()
+connectDb();
 
 export default async (req, res) => {
   try {
-    const { userId } = jwt.verify(req.headers.authorization,
-    process.env.JWT_SECRET
-  );
+    const { userId } = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
     const orders = await Order.find({ user: userId })
-      .sort({ createdAt: 'desc' })
+      .sort({ createdAt: "desc" })
       .populate({
-        path: 'products.product',
+        path: "products.product",
         model: "Product"
       });
-    res.status(200).json({ orders })
-  } catch(error){
-    console.error(error)
-    res.status(403).send("Please login again")
+    res.status(200).json({ orders });
+  } catch (error) {
+    console.error(error);
+    res.status(403).send("Please login again");
   }
-}
+};
